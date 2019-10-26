2df03aad92d02370021985cd7550c8a79076fe15
Lucas Frederico <lucas.frederico@senior.com.br>
2019-10-26 15:17:07 -0300

    implementações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'dev-na-pratica-frontend-basico';
+
+  public marcas: any = [];
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.main();
+  }
+
+  async main() {
+    this.marcas = await this.http.get('http://fipeapi.appspot.com').toPromise();
+
+    const audi = this.marcas.filter((mar: any) => mar.nome === 'Audi')[0];
+
+    console.log(audi);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CardModule} from 'primeng/card';
+import { SharedModule } from './shared/shared.module';
+import { FeatureModule } from './feature/feature.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CardModule,
+    SharedModule,
+    FeatureModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
